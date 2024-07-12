81450aebbd723814dc015397125b5a60fc624668
SEA786 <shams.eltaf786@gmail.com>
2024-07-12 12:56:05 +0200

    01 / 1 Referenztyp(Klasse)

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,15 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+        
+        // Instanziierung  --> Erzeugen eines Objecktes
+        // System.out.println(new Cat());
+       
 
+        // cat --> Referenzvariable
+        // Cat --> Referenztyp(Klasse)
+       Cat cat = new Cat();
+       System.out.println(cat);
 
     }
 
